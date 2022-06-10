9874c16e2ebe34b230474942b767bfc7d638df7d
Pavel Kasila <pavel.kasila@gmail.com>
2022-06-10 22:02:58 +0300

    Upload: new API methods, upload/create SourceUpload (#4)
    * Add ability to upload files (create SourceUpload)
    
    * Remove dropdown from Header
    
    * Add API method to update SourceUpload

---
+++ src/components/uploads/CreateUploadRow.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react';
+import {Button} from "react-bootstrap";
+import API from "../../api";
+import {useHistory} from "react-router-dom";
+
+function CreateUploadRow() {
+    const history = useHistory();
+
+    const [file, setFile] = useState();
+    const [error, setError] = useState();
+
+    return (
+        <tr>
+            <td className="text-truncate align-middle">new</td>
+            <td className="text-truncate align-middle">
+                <input type="file" accept="image/*" onChange={(event) => setFile(event.target.files[0])} />
+            </td>
+            <td className="text-truncate align-middle">{ error ? error.message : ''}</td>
+            <td>
+                <Button variant="outline-secondary" size="sm" onClick={() => {
+                    setError(null);
+                    API.sourceUploads.createUpload(file)
+                        .then(
+                            (result) => {
+                                history.push(`/uploads/${result.id}`);
+                            },
+                            (error) => {
+                                setError(error);
+                            }
+                        );
+                }}>Upload</Button>
+            </td>
+        </tr>
+    );
+}
+
+export default CreateUploadRow;
