341899211914c5f296caba94547ecfb9468ae0ba
Pavel Kasila <pavel.kasila@gmail.com>
2022-06-10 18:37:18 +0300

    Simple pagination

--- src/components/uploads/UploadsPage.js
+++ src/components/uploads/UploadsPage.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import {Container, Alert, Spinner, Pagination} from "react-bootstrap";
+import {Container, Alert, Spinner} from "react-bootstrap";
 import {config} from '../../Constants';
 import UploadsTable from "./UploadsTable";
+import PaginationComponent from "../misc/PaginationComponent";
 
 class UploadsPage extends Component {
     constructor(props) {
@@ -9,12 +10,20 @@ class UploadsPage extends Component {
         this.state = {
             error: null,
             isLoaded: false,
-            items: []
+            items: [],
+            currentPage: 0,
+            page: {size: 20, totalElements: 0, totalPages: 0, number: 0},
         };
+        this.loadPage = this.loadPage.bind(this);
+        this.setPage = this.setPage.bind(this);
     }
 
     componentDidMount() {
-        fetch(`${config.url.API_BASE_URL}/sourceUploads?page=${0}&sort=id,desc`)
+        this.loadPage();
+    }
+
+    loadPage(page) {
+        fetch(`${config.url.API_BASE_URL}/sourceUploads?page=${page ?? this.state.currentPage}&sort=id,desc&size=${this.state.page.size}`)
             .then(res => {
                 if (!res.ok) {
                     throw Error(`${res.status} ${res.statusText}`);
@@ -34,7 +43,8 @@ class UploadsPage extends Component {
                 (result) => {
                     this.setState({
                         isLoaded: true,
-                        items: result.content
+                        items: result.content,
+                        page: result.page
                     });
                 },
                 (error) => {
@@ -44,11 +54,16 @@ class UploadsPage extends Component {
                         items: [],
                     });
                 }
-            )
+            );
+    }
+
+    setPage(page) {
+        this.setState({currentPage: page-1});
+        this.loadPage(page-1);
     }
 
     table() {
-        const { error, isLoaded, items } = this.state;
+        const { error, isLoaded, items, page, currentPage } = this.state;
         if (error) {
             return <Alert variant="danger">Error: {error.message}</Alert>;
         } else if (!isLoaded) {
@@ -57,7 +72,13 @@ class UploadsPage extends Component {
             </Spinner>;
         } else if (items !== undefined) {
             return (
-                <UploadsTable items={items}></UploadsTable>
+                <>
+                    <UploadsTable items={items}></UploadsTable>
+                    <PaginationComponent currentPage={currentPage+1}
+                                         itemsCount={page.totalElements}
+                                         itemsPerPage={page.size}
+                                         setCurrentPage={this.setPage}></PaginationComponent>
+                </>
             );
         } else {
             return <Alert variant="danger">WTF?</Alert>;
