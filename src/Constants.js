0d950782514ac2e1f507b1543a413bab8ef56e8d
Pavel Kasila <pavel.kasila@gmail.com>
2022-06-11 16:44:49 +0300

    Allow setting custom Keycloak and API URLs, use `/api` for production (#9)

--- src/Constants.js
+++ src/Constants.js
@@ -1,10 +1,15 @@
 const prod = {
     url: {
-        KEYCLOAK_BASE_URL: "https://keycloak.pkasila.net/auth",
-        API_BASE_URL: 'https://sources-sf.pkasila.net/api'
+        KEYCLOAK_BASE_URL: process.env.REACT_APP_KEYCLOAK_BASE_URL ?? "https://keycloak.pkasila.net/auth",
+        API_BASE_URL: process.env.REACT_APP_API_BASE_URL ?? '/api'
     }
 }
 
-const dev = prod;
+const dev = {
+    url: {
+        KEYCLOAK_BASE_URL: process.env.REACT_APP_KEYCLOAK_BASE_URL ?? "https://keycloak.pkasila.net/auth",
+        API_BASE_URL: process.env.REACT_APP_API_BASE_URL ?? 'https://sources-sf.pkasila.net/api'
+    }
+};
 
 export const config = process.env.NODE_ENV === 'development' ? dev : prod
