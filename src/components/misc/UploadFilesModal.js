6d4042d296240ef0924bd502bb6cb1739beb9aa5
Pavel Kasila <pavel.kasila@gmail.com>
2022-07-02 13:50:47 +0300

    Source files API (#32)
    * Update APIs, fix timeout bug in UploadEditPage, allow uploading multiple files initially

--- src/components/misc/UploadFilesModal.js
+++ src/components/misc/UploadFilesModal.js
@@ -1,36 +1,36 @@
 import React, {Component} from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import {Modal, Button} from 'react-bootstrap';
 
-class UploadFileModal extends Component {
+class UploadFilesModal extends Component {
     constructor(props) {
         super(props);
 
-        this.state={
-            file: null
+        this.state = {
+            files: null
         }
-        
-        this.uploadFile = this.uploadFile.bind(this);
+
+        this.uploadFiles = this.uploadFiles.bind(this);
     }
 
-    uploadFile() {
-        //Method for uploading file
+    uploadFiles() {
+        // Method for uploading file
     }
 
     render() {
-        return(
+        return (
             <Modal show={this.props.showModal} onHide={this.props.closeModal}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Add file to upload</Modal.Title>
+                    <Modal.Title>Add files to upload</Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
                     <input type="file" accept="image/png,image/jpeg,image/gif,application/pdf"
-                        onChange={(event) => this.setState({file: event.target.files[0]})} />
+                           onChange={(event) => this.setState({files: Array.from(event.target.files)})}/>
                 </Modal.Body>
 
                 <Modal.Footer>
-                    <Button variant="outline-success" onClick={this.uploadFile}>
-                        Upload
+                    <Button variant="outline-success" onClick={this.uploadFiles}>
+                        Save
                     </Button>
                     <Button variant="outline-secondary" onClick={this.props.closeModal}>
                         Cancel
@@ -41,4 +41,4 @@ class UploadFileModal extends Component {
     }
 }
 
-export default UploadFileModal;
+export default UploadFilesModal;
